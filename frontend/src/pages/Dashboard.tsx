82a3da90507de621d64a79e79a6e1c3d88d10cc5
saurabh <saurabhmehta1601@gmail.com>
2023-02-08 11:27:54 +0530

    WIP(Dashboard): add active state to dashboard menu

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
 import styles from "../styles/dashboard.module.scss"
 import zocketLogo from "../assets/zocket-logo.svg"
-import homeIcon from "../assets/icons/home.svg"
-import campaignIcon from "../assets/icons/campaign.svg"
-import customersIcon from "../assets/icons/customers.svg"
-import productsIcon from "../assets/icons/products.svg"
+import homeIconOutlined from "../assets/icons/outlined/home.svg"
+import campaignIconOutlined from "../assets/icons/outlined/campaign.svg"
+import customersIconOutlined from "../assets/icons/outlined/customers.svg"
+import productsIconOutlined from "../assets/icons/outlined/products.svg"
+import homeIconSolid from "../assets/icons/solid/home.svg"
+import campaignIconSolid from "../assets/icons/solid/campaign.svg"
+import customersIconSolid from "../assets/icons/solid/customers.svg"
+import productsIconSolid from "../assets/icons/solid/products.svg"
+
 
 enum MenuItemLabel {
     HOME = "Home", CAMPAIGN = "Campaign", PRODUCTS = "Products", CUSTOMERS = "Customers"
@@ -15,11 +20,12 @@ interface IMenuItem {
     isActiveMenuItem: boolean,
     label: string
     iconSrc: string,
+    activeIconSrc: string
     onClick: () => void
 }
-const MenuItem = ({ isActiveMenuItem, label, iconSrc, onClick }: IMenuItem) => (
+const MenuItem = ({ isActiveMenuItem, label, iconSrc, onClick, activeIconSrc }: IMenuItem) => (
     <div className={[styles.menuItem, isActiveMenuItem ? styles.activeMenuItem : ""].join(" ")} onClick={onClick}>
-        <img src={iconSrc} alt={label} className={styles.menuIcon} />
+        <img src={isActiveMenuItem ? activeIconSrc : iconSrc} alt={label} className={styles.menuIcon} />
         <p className={styles.label}>{label}</p>
     </div>
 )
@@ -31,25 +37,29 @@ const Dashboard = () => {
             <div className={styles.menu}>
                 <img src={zocketLogo} alt="zocket logo" className={styles.zocketLogo} />
                 <MenuItem
-                    iconSrc={homeIcon}
+                    iconSrc={homeIconOutlined}
+                    activeIconSrc={homeIconSolid}
                     label={MenuItemLabel.HOME}
                     onClick={() => setSelectedMenuItem(MenuItemLabel.HOME)}
                     isActiveMenuItem={selectedMenuItem === MenuItemLabel.HOME}
                 />
                 <MenuItem
-                    iconSrc={campaignIcon}
+                    iconSrc={campaignIconOutlined}
+                    activeIconSrc={campaignIconSolid}
                     label={MenuItemLabel.CAMPAIGN}
                     onClick={() => setSelectedMenuItem(MenuItemLabel.CAMPAIGN)}
                     isActiveMenuItem={selectedMenuItem === MenuItemLabel.CAMPAIGN}
                 />
                 <MenuItem
-                    iconSrc={productsIcon}
+                    iconSrc={productsIconOutlined}
+                    activeIconSrc={productsIconSolid}
                     label={MenuItemLabel.PRODUCTS}
                     onClick={() => setSelectedMenuItem(MenuItemLabel.PRODUCTS)}
                     isActiveMenuItem={selectedMenuItem === MenuItemLabel.PRODUCTS}
                 />
                 <MenuItem
-                    iconSrc={customersIcon}
+                    iconSrc={customersIconOutlined}
+                    activeIconSrc={customersIconSolid}
                     label={MenuItemLabel.CUSTOMERS}
                     onClick={() => setSelectedMenuItem(MenuItemLabel.CUSTOMERS)}
                     isActiveMenuItem={selectedMenuItem === MenuItemLabel.CUSTOMERS}
