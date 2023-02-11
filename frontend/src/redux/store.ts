2117969794a8446f148b0c29e55eb3f55b88e415
saurabh <saurabhmehta1601@gmail.com>
2023-02-11 10:06:34 +0530

    refactor: remove redux-persist

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,36 +1,20 @@
 import {
-  combineReducers,
   configureStore,
   getDefaultMiddleware,
 } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import createCampaignSlice from "./features/createCampaignSlice";
-import dashboardSlice from "./features/dashboardSlice";
-
-const persistConfig = {
-  key: "root",
-  storage,
-};
-
-const rootReducer = combineReducers({
-  // Add your reducers here
-  dashboard: dashboardSlice,
-  createCampaign: createCampaignSlice,
-});
-
-export default rootReducer;
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+import createCampaignReducer from "./features/createCampaignSlice";
+import dashboardReducer from "./features/dashboardSlice";
 
 const store = configureStore({
-  reducer: persistedReducer,
+  reducer: {
+    dashboard: dashboardReducer,
+    createCampaign: createCampaignReducer,
+  },
   middleware: getDefaultMiddleware({
     serializableCheck: false,
   }),
 });
 
-const persistor = persistStore(store);
-
-export { store, persistor };
-export type RootState = ReturnType<typeof rootReducer>;
+export { store };
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
